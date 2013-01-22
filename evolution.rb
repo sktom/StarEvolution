f2bbca7bd7b53625310c8c05f7cbb877b90a9ee6
Tomonori Konno <sktomorg@gmail.com>
2013-01-22 09:02:09 +0900

    fix diff-macro's bug

--- evolution.rb
+++ evolution.rb
@@ -1,48 +1,45 @@
 
 module Evolution
 
-  module SimpleMrEvolution
-    def dP(rad, m_r)
+    def dPdMr_(rad, m_r)
+      - @G * m_r / (4 * @pi * @radius ** 4)
     end
-    def drho(temp, rho, comp)
-    end
-    def dtemp(m_r, temp, rad, comp)
-    end
-    def dL(temp, rho, comp)
-    end
-    def dr(rad, rho)
-    end
-  end
-
-  module MrEvolution
     def press(rho, temp)
     end
-    def dP(rad, m_r)
+    def drhodMr(temp, rho, comp)
+      @dMr / (4 * @pi * @radius**2 * dr)
     end
-    def drho(temp, rho, comp)
+    def dtempdMr(m_r, temp, rad, comp)
+      -(1 - @gamma**-1) * @G * m_r * @temperature / (4 * @pi * @radius**4 * @pressure)
     end
-    def dtemp(m_r, temp, rad, comp)
+    def dLdMr(temp, rho, comp)
+      t9 = @temperature / 10**9
+      e_pp = 2.4 * 10**4 * @density * @hydrogen**2 / t9**(2.0 / 3) * Math::exp(-3.380 / t9**(3**-1))
+      e_cno = 4.4 * 10**25 * @density * @hydrogen * @metal / t9**(2.0 / 3) * Math::exp(-15.228 / t9**(3**-1))
+      e_n = 0
+      e_pp + e_cno - e_n
     end
-    def dL(temp, rho, comp)
+    def drdMr(rad, rho)
+      @dMr * (4 * @pi * @radius**2 * @density)**-1
     end
-    def dr(rad, rho)
-    end
-  end
 
-  module TimeEvolution
-    def dcomp(temp, rho, comp)
+    def dcompdt(temp, rho, comp)
     end
-    def dP(rad, m_r)
+    def dPdt(rad, m_r)
     end
-    def drho(temp, rho, comp)
+    def drhodt(temp, rho, comp)
     end
-    def dtemp(m_r, temp, rad, comp)
+    def dtempdt(m_r, temp, rad, comp)
     end
-    def dL(temp, rho, comp)
+    def dLdt(temp, rho, comp)
     end
-    def dr(rad, rho)
+    def drdt(rad, rho)
     end
-  end
 
+    private
+    def g()
+    end
+    def g_()
+    end
 
 end
